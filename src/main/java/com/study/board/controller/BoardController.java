975328dc8bc71b973ee15f9456509b90abd5c762
leessoo67 <qkrtjdwk67@naver.com>
2024-10-04 18:11:09 +0900

    3

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,6 +35,17 @@ public class BoardController {
 
       return"";
   }
+  //1
+  @GetMapping("/board/list")//localhost:8080/board/list 로 접속하면 보여
+  public String boardList(Model model) { //4 데이터를 담아서 보여줘야 할 때 쓰는 model
+
+
+      model.addAttribute("list", boardService.boardList()); //list라는이름으로 보낼건데 뭘 담아 보낼거냐면
+    //boardservice에 boardlist가 실행되면 list가 반환이 되는데 그 list를 "list"로 받아서 넘기겠다.
+
+      return "boardlist"; //boardlist.html을 만들어줘야함.
+  }
+
 
 
 }
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -5,6 +5,9 @@ import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 //boardcontroller로 이동
 @Service
 public class BoardService {
@@ -13,9 +16,16 @@ public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
 
-    public void write(Board board) {
+    public void write(Board board) { //Board 엔티티 객체를 받아서 데이터베이스에 저장
 
         boardRepository.save(board);
+
+    }
+
+    //3 글을 불러와야 하니까 서비스에서 처리
+    public List<Board> boardList() {// list에 빨간색이 뜨면 컨트롤 스페이스
+
+        return boardRepository.findAll();
 
     }
 }
